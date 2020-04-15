67f71950379fc6dc0994fe5e1c0f4446f580ddcc
Клюжев И. А <klyuzhevigor@gmail.com>
2020-04-15 15:46:15 +0300

    Added URLSession and parsing json

--- API-test/ViewController.swift
+++ API-test/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchBar.delegate = self
     }
 
 
 }
 
+extension ViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let apiKey = "7e5c9bf9245f103431d12d0c6cf17fa0"
+        let urlString = "http://api.weatherstack.com/current?access_key=\(apiKey)&query=\(searchBar.text!)"
+        let url = URL(string: urlString)
+        
+        var locationName: String?
+        var temperature: Double?
+        
+        let task = URLSession.shared.dataTask(with: url!) {
+            (data, response, error) in
+            
+            do {
+                let json = try  JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String : AnyObject]
+                
+                if let location = json["location"] {
+                    locationName = location["name"] as? String
+                }
+                
+                if let current = json["current"] {
+                    temperature = current["temperature"] as? Double
+                }
+                
+            }
+            catch let jsonError{ 
+                print(jsonError)
+            }
+        }
+        task.resume()
+    }
+}
