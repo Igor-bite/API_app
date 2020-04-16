333f3396d64a11217ede4b069a9a9fb67b09931c
Клюжев И. А <klyuzhevigor@gmail.com>
2020-04-16 16:30:58 +0300

    functions made

--- API-test/ViewController.swift
+++ API-test/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var windSpeedLabel: UILabel!
     @IBOutlet weak var visibilityLabel: UILabel!
     @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var segmentTemp: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,17 +25,33 @@ class ViewController: UIViewController {
 
 }
 
+open class UIControlEventValueChanged {
+    func segmentChanged(_ segmentTemp: UISegmentedControl ) {
+        if (segmentTemp.selectedSegmentIndex == 1) {
+            
+        } else if (segmentTemp.selectedSegmentIndex == 2) {
+            
+        } else if (segmentTemp.selectedSegmentIndex == 3) {
+            
+        }
+    }
+}
+
 extension ViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         searchBar.resignFirstResponder()
         
+        segmentTemp.selectedSegmentIndex = 1
+        
+        self.parse_ID();
+    }
+    
+    func parse_ID() {
         let urlString = "https://www.metaweather.com/api/location/search/?query=\(searchBar.text!.replacingOccurrences(of: " ", with: "%20"))"
         let url = URL(string: urlString)
         
-         
         var locationName: String?
         var locationID: Int?
-        var errorHasOccured: Bool = false
         
         let task = URLSession.shared.dataTask(with: url!) { [weak self] (data, response, error) in
             do {
@@ -51,57 +68,8 @@ extension ViewController: UISearchBarDelegate {
                     
                     let mainUrlString = "https://www.metaweather.com/api/location/\(locationID!)"
                     let mainUrl = URL(string: mainUrlString)
-                    var temperature: Double?
-                    var humidity: Double?
-                    var visibility: Double?
-                    var wind_speed: Double?
                     
-                    let mainTask = URLSession.shared.dataTask(with: mainUrl!) { [weak self] (data1, response1, error1) in
-                        do {
-                            
-                            let mainJson = try JSONSerialization.jsonObject(with: data1!, options: .mutableContainers)
-                                as! [String : AnyObject]
-                            
-                            if mainJson.isEmpty {
-                                errorHasOccured = true
-                            }
-                            
-                            let consolidated_weather = mainJson["consolidated_weather"] as! [[String : AnyObject]]?
-                            temperature = consolidated_weather?[0]["the_temp"] as! Double?
-                            humidity = consolidated_weather?[0]["humidity"] as! Double?
-                            visibility = consolidated_weather?[0]["visibility"] as! Double?
-                            wind_speed = consolidated_weather?[0]["wind_speed"] as! Double?
-                            /*
-                            "min_temp"
-                            "max_temp"
-                            "the_temp"
-                            "wind_speed"
-                            "wind_direction"
-                            "air_pressure"
-                            "humidity"
-                            "visibility"
-                             */
-                            DispatchQueue.main.async {
-                                if errorHasOccured{
-                                    self?.cityLabel.text = "Not found"
-                                    self?.tempLabel.isHidden = true
-                                } else {
-                                    self?.cityLabel.text = locationName!
-                                    self?.tempLabel.text = "\(round(temperature!*10)/10) C"
-                                    self?.humidityLabel.text = "\(humidity!)%"
-                                    self?.windSpeedLabel.text = "\(round(wind_speed!*10)/10) m/s"
-                                    self?.visibilityLabel.text = "\(round(visibility!*10)/10) m"
-                                    self?.tempLabel.isHidden = false
-                                    self?.searchBar.isTranslucent = true
-                                }
-                            }
-                            
-                        }
-                        catch let jsonError1 {
-                            print(jsonError1)
-                        }
-                    }
-                    mainTask.resume()
+                    self?.parse_temp(locationName: locationName, mainUrl: mainUrl);
                 }
             }
             catch let jsonError {
@@ -110,7 +78,121 @@ extension ViewController: UISearchBarDelegate {
         }
         task.resume()
     }
-    func parsing(url: URL) {
-        print("\(url)")
+    
+    func parse_temp(locationName: String?, mainUrl: URL?) {
+        var temperature: Double?
+        var humidity: Double?
+        var visibility: Double?
+        var wind_speed: Double?
+        var errorHasOccured: Bool = false
+        
+        let mainTask = URLSession.shared.dataTask(with: mainUrl!) { [weak self] (data1, response1, error1) in
+            do {
+                
+                let mainJson = try JSONSerialization.jsonObject(with: data1!, options: .mutableContainers)
+                    as! [String : AnyObject]
+                
+                if mainJson.isEmpty {
+                    errorHasOccured = true
+                }
+                
+                let consolidated_weather = mainJson["consolidated_weather"] as! [[String : AnyObject]]?
+                temperature = consolidated_weather?[0]["the_temp"] as! Double?
+                humidity = consolidated_weather?[0]["humidity"] as! Double?
+                visibility = consolidated_weather?[0]["visibility"] as! Double?
+                wind_speed = consolidated_weather?[0]["wind_speed"] as! Double?
+                
+                self?.Display(locationName: locationName, temperature: temperature, humidity: humidity, visibility: visibility, wind_speed: wind_speed, errorHasOccured: errorHasOccured);
+                
+            }
+            catch let jsonError1 {
+                print(jsonError1)
+            }
+        }
+        mainTask.resume()
+    }
+    
+    func parse_min_temp(locationName: String?, mainUrl: URL?) {
+        var temperature: Double?
+        var humidity: Double?
+        var visibility: Double?
+        var wind_speed: Double?
+        var errorHasOccured: Bool = false
+
+        let mainTask = URLSession.shared.dataTask(with: mainUrl!) { [weak self] (data1, response1, error1) in
+            do {
+                
+                let mainJson = try JSONSerialization.jsonObject(with: data1!, options: .mutableContainers)
+                    as! [String : AnyObject]
+                
+                if mainJson.isEmpty {
+                    errorHasOccured = true
+                }
+                
+                let consolidated_weather = mainJson["consolidated_weather"] as! [[String : AnyObject]]?
+                temperature = consolidated_weather?[0]["min_temp"] as! Double?
+                humidity = consolidated_weather?[0]["humidity"] as! Double?
+                visibility = consolidated_weather?[0]["visibility"] as! Double?
+                wind_speed = consolidated_weather?[0]["wind_speed"] as! Double?
+                
+                self?.Display(locationName: locationName, temperature: temperature, humidity: humidity, visibility: visibility, wind_speed: wind_speed, errorHasOccured: errorHasOccured);
+                
+            }
+            catch let jsonError1 {
+                print(jsonError1)
+            }
+        }
+        mainTask.resume()
+    }
+    
+    func parse_max_temp(locationName: String?, mainUrl: URL?) {
+            var temperature: Double?
+            var humidity: Double?
+            var visibility: Double?
+            var wind_speed: Double?
+            var errorHasOccured: Bool = false
+
+            let mainTask = URLSession.shared.dataTask(with: mainUrl!) { [weak self] (data1, response1, error1) in
+                do {
+                    
+                    let mainJson = try JSONSerialization.jsonObject(with: data1!, options: .mutableContainers)
+                        as! [String : AnyObject]
+                    
+                    if mainJson.isEmpty {
+                        errorHasOccured = true
+                    }
+                    
+                    let consolidated_weather = mainJson["consolidated_weather"] as! [[String : AnyObject]]?
+                    temperature = consolidated_weather?[0]["max_temp"] as! Double?
+                    humidity = consolidated_weather?[0]["humidity"] as! Double?
+                    visibility = consolidated_weather?[0]["visibility"] as! Double?
+                    wind_speed = consolidated_weather?[0]["wind_speed"] as! Double?
+                    
+                    self?.Display(locationName: locationName, temperature: temperature, humidity: humidity, visibility: visibility, wind_speed: wind_speed, errorHasOccured: errorHasOccured);
+                    
+                }
+                catch let jsonError1 {
+                    print(jsonError1)
+                }
+            }
+            mainTask.resume()
+        }
+    
+    func Display(locationName: String?, temperature: Double?,humidity: Double?,visibility: Double?,wind_speed: Double?,errorHasOccured: Bool) {
+        DispatchQueue.main.async {
+            if errorHasOccured{
+                self.cityLabel.text = "Not found"
+                self.tempLabel.isHidden = true
+            } else {
+                self.cityLabel.text = locationName!
+                self.tempLabel.text = "\(round(temperature!*10)/10) C"
+                self.humidityLabel.text = "\(humidity!)%"
+                self.windSpeedLabel.text = "\(round(wind_speed!*10)/10) m/s"
+                self.visibilityLabel.text = "\(round(visibility!*10)/10) m"
+                self.tempLabel.isHidden = false
+            }
+        }
     }
 }
+
+
